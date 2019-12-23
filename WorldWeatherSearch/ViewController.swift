da6c496ae1dcf1486cba67effc07f58e3445ea54
RockingPhan <phani.deepak8@gmail.com>
2019-12-23 12:34:56 +0530

    App Initial Setup
    This commit add files with app initial setup

---
+++ WorldWeatherSearch/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WorldWeatherSearch
+//
+//  Created by Phanindra Kumar  on 23/12/19.
+//  Copyright © 2019 Phanindra Kumar . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("In view controller")
+    }
+
+
+}
+
