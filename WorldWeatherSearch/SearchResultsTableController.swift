579e7f5b68ca8fe04a4cda63511d0324f1c31b82
Phanindra Kumar <phani.deepak8@gmail.com>
2019-12-23 14:10:55 +0530

    Added Search bar controller and results table view controller designs to show the search results

---
+++ WorldWeatherSearch/SearchResultsTableController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchResultsTableController.swift
+//  WorldWeatherSearch
+//
+//  Created by Phanindra Kumar  on 23/12/19.
+//  Copyright © 2019 Phanindra Kumar . All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsTableController: UITableViewController {
+    
+    
+    
+}
+
+extension SearchResultsTableController : UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+    }
+    
+}
--- WorldWeatherSearch/ViewController.swift
+++ WorldWeatherSearch/ViewController.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var searchController:UISearchController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("In view controller")
+        
+        let searchResultsTableController = storyboard!.instantiateViewController(withIdentifier: "SearchResultsTableController") as! SearchResultsTableController
+        searchController = UISearchController(searchResultsController: searchResultsTableController)
+        searchController.searchResultsUpdater = searchResultsTableController as UISearchResultsUpdating
+        
+        searchController.searchBar.sizeToFit()
+        searchController.searchBar.placeholder = "Search for places"
+        
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        
     }
 
 
